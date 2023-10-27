4672faea86b2dc12e6ec136b311722dd5e16a91c
b4lmor <artem.lisitsin.02@mail.ru>
2023-10-27 21:47:30 +0700

    ...

--- LAB2/src/main.cpp
+++ LAB2/src/main.cpp
@@ -3,10 +3,10 @@
 #include "renderer/GameRenderer.h"
 #include <memory>
 
-//int main(int argc, char * argv[]) {
-//    GameArgs args = GameArgs(argc, argv);
-//    std::unique_ptr<AGame> game(get_game_by_args(args));
-//    games_t games = game->run_game();
-//    GameRenderer::render_game(args, games);
-//	return 0;
-//}
+int main(int argc, char * argv[]) {
+    GameArgs args = GameArgs(argc, argv);
+    std::unique_ptr<AGame> game(get_game_by_args(args));
+    games_t games = game->run_game();
+    GameRenderer::render_game(args, games);
+	return 0;
+}
